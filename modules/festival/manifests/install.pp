d9ddc8ba108591103da53d28a735a39c7f825637
Nathan Howard <nathan@freeagent.com>
2013-09-27 13:51:11 +0100

    Add more voices to festival

--- modules/festival/manifests/install.pp
+++ modules/festival/manifests/install.pp
@@ -2,4 +2,11 @@ class festival::install {
   package {'festival':
     ensure => 'installed',
   }
+
+  # Install some voices
+  package {
+    'festvox-don': ensure       => 'installed';
+    'festvox-rablpc16k': ensure => 'installed';
+    'festlex-oald': ensure      => 'installed';
+  }
 }
