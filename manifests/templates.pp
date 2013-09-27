ab03cd3b63371268abe3ee8f77c6cfdefa6618cf
Nathan Howard <nathan@freeagent.com>
2013-09-27 13:56:38 +0100

    Install sqlite3 by default

--- manifests/templates.pp
+++ manifests/templates.pp
@@ -2,6 +2,7 @@ class base {
   package {
     'git-core':       ensure => 'installed';
     'screen':         ensure => 'installed';
+    'sqlite3':        ensure => 'installed';
     'libsqlite3-dev': ensure => 'installed';
     # Required to build listener.c
     'libasound2-dev': ensure => 'installed';
